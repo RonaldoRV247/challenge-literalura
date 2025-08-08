a673785233266db8e87fd9beb5711536f5ae9531
Ronaldo <vergaray247@gmail.com>
2025-08-07 23:24:04 -0500

    Correccion API

--- src/main/java/com/example/literalura_challenge/principal/Principal.java
+++ src/main/java/com/example/literalura_challenge/principal/Principal.java
@@ -17,7 +17,7 @@ public class Principal {
     private Scanner teclado = new Scanner(System.in);
     private ConsumoAPI consumoApi = new ConsumoAPI();
     private ConvierteDatos conversor = new ConvierteDatos();
-    private final String URL_BASE = "https://gutendx.com/books/";
+    private final String URL_BASE = "https://gutendex.com/books/";
 
     @Autowired
     private LibroRepository libroRepository;
